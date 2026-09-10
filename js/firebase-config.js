2b4c9417297d69d817e307cb5de48cbeb62b2b5b
shfthof <shftdev@gmail.com>
2026-09-10 10:20:22 +0300

    Refactor Firebase config and initialize analytics
    Updated Firebase configuration to use specific SDK imports and added analytics initialization.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,25 +1,22 @@
-/**
- * CubicLens - Firebase Configuration
- * Initialize Firebase and export database reference
- */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase SDK imports (using CDN)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import { getDatabase, ref, set, push, onValue, remove, update } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-database.js";
-
-// Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDoxk81n4RuIi-gTj6a-MEcEH5qEBxslOc",
   authDomain: "cubiclensweb.firebaseapp.com",
+  databaseURL: "https://cubiclensweb-default-rtdb.firebaseio.com",
   projectId: "cubiclensweb",
   storageBucket: "cubiclensweb.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "160780771485",
+  appId: "1:160780771485:web:fc8b2ffa7f93a1442a7474",
+  measurementId: "G-517EN98DCR"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// Export Firebase functions and references
-export { db, ref, set, push, onValue, remove, update };
+const analytics = getAnalytics(app);
